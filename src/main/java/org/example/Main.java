14d2237634cc3b532ebee18d6f12b93cba3639f5
WaleriiChirkov-16021990 <zx84058405@live.com>
2023-11-20 22:50:16 +0300

    final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        new Paradox(100000).getStatisticsParadoxIfDoNotChangeChoice();
-        new Paradox(100000).getStatisticsParadoxIfChangeChoice();
+        try {
+            new Paradox(500).getStatisticsParadoxIfDoNotChangeChoice();
+            new Paradox(500).getStatisticsParadoxIfChangeChoice();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
     }
 }
